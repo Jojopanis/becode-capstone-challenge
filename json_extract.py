aa237c29632a229669cbee43f521fdd525affb0a
Geoffroy Panis <jojopanis@gmail.com>
2024-11-05 20:00:32 +0100

    cleaned the scrapper code

--- json_extract.py
+++ json_extract.py
@@ -1,16 +1,6 @@
 import requests
 import json
 
-def cycle_pages(number_of_pages):
-    all_articles = []
-    for i in range(number_of_pages):
-        url_json = f"https://bff-service.rtbf.be/oaos/v1.5/pages/en-continu?_page={i+1}&_limit=100"
-        page_json = requests.get(url_json).text
-        print(f"Scraping page {i+1}...")
-        article_list = json.loads(page_json)["data"]["articles"]
-        all_articles.extend(scrap_page(article_list))
-    return all_articles
-
 def scrap_page(article_list):
     data_articles = []
     for article in article_list:
@@ -23,7 +13,20 @@ def scrap_page(article_list):
         data_articles.append(article_data)
     return data_articles
 
+def cycle_pages(number_of_pages):
+    all_articles = []
+    for i in range(number_of_pages):
+        url_json = f"https://bff-service.rtbf.be/oaos/v1.5/pages/en-continu?_page={i+1}&_limit=100"
+        page_json = requests.get(url_json).text
+        print(f"Scraping page {i+1}...")
+        article_list = json.loads(page_json)["data"]["articles"]
+        all_articles.extend(scrap_page(article_list))
+    return all_articles
+
+def write_json(number_of_pages:int = 100, filename='articles.json'):
+    data = cycle_pages(number_of_pages)
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=2)
+
 if __name__ == "__main__":
-    with open('articles.json', 'w') as f:
-        json.dump(cycle_pages(100), f, indent=2)
-    
+    write_json(100)

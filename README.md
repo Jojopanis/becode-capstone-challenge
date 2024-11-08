b09dc91fc45929c5a0dd2798517f75c4f7e420d8
Geoffroy Panis <jojopanis@gmail.com>
2024-11-08 17:05:54 +0100

    tried the taipy implementation

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# becode-capstone-challenge
+# BeCode Capstone Challenge
+
+
+![Static Badge](https://img.shields.io/badge/python-3.12-green?logo=python&color=%23FFE873) 
+![Static Badge](https://img.shields.io/badge/plotly%20-%205.24.1%20-%20%233F4F75?logo=plotly&logoColor=%233F4F75)
+![Static Badge](https://img.shields.io/badge/taipy%20-%204.0.0%20-%20%23FF371A?logo=taipy&logoColor=%23FF371A)
+
+
+
+## Overview
+
+The scope of this project is to summarize all the knowledge we got from our training at [BeCode](https://becode.org/) in one big project. The main objective is to get informations form a news site, cleaning and pre-processing it to feed them into a model that will cluster them based on topic modeling. And ideally, deplying the whole project online.
+
+## Usage
+
+## Timeline
+
+Start of the first week - We spent some time to explore the stucture of the [rtbf website](https://www.rtbf.be/en-continu) and found a json structure to easily scrap
+
+## Roadblocks
+
+The first roadblock we found was the rtbf news API wich, with the method we used, limited us to the number 100. So we had to maximise the scrapping to 100 pages of 100 articles each.
---
+++ utils/json_extract.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+
+def scrap_page(page_number):
+    article_list = []
+    url_json = f"https://bff-service.rtbf.be/oaos/v1.5/pages/en-continu?_page={page_number}&_limit=100"
+    page_json = requests.get(url_json).text
+    article_list = json.loads(page_json)["data"]["articles"]
+    data_articles = []
+    for article in article_list:
+        article_data = {
+            "id": article["id"],
+            "slug": article["slug"],
+            "title": article["title"],
+            "category": article["dossierLabel"],
+            "date": article["publishedFrom"],
+            "update": article["majorUpdatedAt"]}
+        data_articles.append(article_data)
+    return data_articles
+
+def write_json(result, filename='./data/articles.json'):
+    data = []
+    for r in result:
+        data.extend(r)
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=2)
+
+if __name__ == "__main__":
+    with ThreadPoolExecutor() as executor:
+        result = list(tqdm(executor.map(scrap_page, range(1, 101)), total=100))
+    write_json(result)
